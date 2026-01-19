655f249be659c33bcb063ec4bc92f9023af96b6b
sahuajith <ajith.sahu1@gmail.com>
2026-01-19 20:10:22 +0530

    Update check_env.py

--- scripts/check_env.py
+++ scripts/check_env.py
@@ -1,15 +1,8 @@
 import os
 import sys
-required_vars=["DB_PASSWORD"]
-missing_vars=[]
-for var in required_vars:
-    if os.getenv(var) is None:
-        missing_vars.append(var)
-if missing_vars:
+if "DB_PASSWORD" in os.environ:
     print("Error")
-    for var in missing_vars:
-        print(f"-{var}")
     sys.exit(1)
 else:
-    print("Success")
+    print("Present")
     sys.exit(0)
